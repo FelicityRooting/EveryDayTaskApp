25097f5d95ed24b63d68bdaa273fd4c74ba7e27b
Felicity Liu <FelicityRooting@users.noreply.github.com>
2018-08-29 15:46:43 -0400

    add setup animation

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import { LocalStorageService } from "../../service/local-storage.service";
 import {
   USERNAME,
@@ -6,17 +6,22 @@ import {
   START_USING_DATE
 } from "../../service/local-storage.namespace";
 import { getTodayTime } from '../../../utils/time';//用时间戳来简单地处理时间
+import { setupTransition } from './setup.animation';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.less']
+  styleUrls: ['./setup.component.less'],
+  animations: [ setupTransition ]
 })
 export class SetupComponent implements OnInit {
+  @HostBinding('@setupTransition') state = 'activated';
   username: string;
 
   constructor(
-    private store: LocalStorageService
+    private store: LocalStorageService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -26,6 +31,8 @@ export class SetupComponent implements OnInit {
     this.store.set(INIT_FLAG, true);
     this.store.set(START_USING_DATE, getTodayTime());
     this.store.set(USERNAME, this.username);
+
+    this.router.navigateByUrl('main');
   }
 
 }
